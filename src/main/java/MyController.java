c553c1aa7bc62f8672f02942c243a0cb63664cf9
GreenPiggies <hungwesley@gmail.com>
2020-06-18 20:41:16 -0700

    Comments, fixed issue with 503 error on crawling Amazon links.

--- src/main/java/MyController.java
+++ src/main/java/MyController.java
@@ -6,38 +6,55 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * The MyController class is the "runner" class for my web crawler.
+ */
 public class MyController {
     public static void main(String[] args) throws Exception {
         CrawlConfig config = new CrawlConfig();
 
+        config.setUserAgentString("intern bot");
+
+        // this folder stores all the data that the crawler needs to store
         config.setCrawlStorageFolder("/tmp/amazon-web-crawler/");
 
-        config.setPolitenessDelay(1000);
+        // a politeness delay, to make sure we don't ping the servers too much
+        config.setPolitenessDelay(5000);
 
+        // how far we can crawl from the "seed" pages (think DFS depth)
         config.setMaxDepthOfCrawling(2);
 
+        // max number of pages we can fetch
         config.setMaxPagesToFetch(1000);
 
+        // binary content includes pictures, etc etc.
         config.setIncludeBinaryContentInCrawling(false);
 
+        // resumable crawling is for starting/stopping the crawler.
         config.setResumableCrawling(false);
 
+        // setting up all the objects we need to start our controller
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+
+        // instantiate our controller
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-        controller.addSeed("https://www.amazon.com/Echo-Dot/dp/B07PDHSLM6/");
+        // add our controller seed (start page)
+        controller.addSeed("https://www.amazon.com/dp/B07FZ8S74R/");
 
-        int numberOfCrawlers = 8; // number of threads used during crawling
+        // number of threads used during crawling
+        int numberOfCrawlers = 8;
 
+        // construct a new AtomicInteger, which we will use to keep track of the number of pages seem.
         AtomicInteger numPagesSeen = new AtomicInteger();
 
+        // create our factory of web crawlers
         CrawlController.WebCrawlerFactory<MyCrawler> factory = () -> new MyCrawler(numPagesSeen);
 
+        // start crawling
         controller.start(factory, numberOfCrawlers);
-
-
 
     }
 }
--- src/main/java/MyCrawler.java
+++ src/main/java/MyCrawler.java
@@ -8,29 +8,50 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
+/**
+ * The MyCrawler class constructs an Amazon Web Crawler that keeps track of the number of pages it has crawled through.
+ */
 public class MyCrawler extends WebCrawler {
 
+    // not being used atm, might remove it later
     private static final Pattern WEBSITE_EXTENSIONS = Pattern.compile(".*\\.(org|com|gov)$");
 
+    // keeps count on the number of crawled (seen) pages
     private final AtomicInteger seenPages;
 
+    /**
+     * Constructs a MyCrawler object.
+     * @param pages An AtomicInteger that keeps track of the number of pages visited by the crawler.
+     */
     public MyCrawler(AtomicInteger pages) {
         seenPages = pages;
     }
 
+    /**
+     * Returns whether or not the specified URL should be visited, given the page it has been found in.
+     * @param referringPage The page from which the specified URL was found.
+     * @param url The specified URL.
+     * @return True if the URL should be visited, false otherwise.
+     */
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
+        // atm referringPage isn't used, might be used later
         String href = url.getURL().toLowerCase();
 
-        if (WEBSITE_EXTENSIONS.matcher(href).matches() && href.startsWith("https://www.amazon.com/")) {
-            seenPages.incrementAndGet();
-            return true;
-        }
-        return false;
+        // pretty simple heuristic, visit the page if its an amazon link.
+        return href.startsWith("https://www.amazon.com/");
     }
 
+    /**
+     * Visits a web page.
+     * @param page The page to be visited.
+     */
     @Override
     public void visit(Page page) {
+
+        /* A lot of this website data isn't used right now, I'm in the process of going through these and figuring out exactly what they are.
+         * For now, I'm just printing them out on the logger.
+         * */
         int docid = page.getWebURL().getDocid();
         String url = page.getWebURL().getURL();
         String domain = page.getWebURL().getDomain();
@@ -47,10 +68,13 @@ public class MyCrawler extends WebCrawler {
         logger.debug("Parent page: {}", parentUrl);
         logger.debug("Anchor text: {}", anchor);
 
+        // making sure the webpage is parseable
         if (page.getParseData() instanceof HtmlParseData) {
+
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
             String text = htmlParseData.getText();
             String html = htmlParseData.getHtml();
+            // this code gets the outgoing URLs, which we want to crawl
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
             logger.debug("Text length: {}", text.length());
@@ -58,6 +82,7 @@ public class MyCrawler extends WebCrawler {
             logger.debug("Number of outgoing links: {}", links.size());
         }
 
+        // some metadata about the page
         Header[] responseHeaders = page.getFetchResponseHeaders();
         if (responseHeaders != null) {
             logger.debug("Response headers:");
@@ -65,6 +90,9 @@ public class MyCrawler extends WebCrawler {
                 logger.debug("\t{}: {}", header.getName(), header.getValue());
             }
         }
+
+        // printout for the number of pages visited so far.
+        logger.debug("Pages visited: " + seenPages.incrementAndGet());
 
         logger.debug("=============");
     }
